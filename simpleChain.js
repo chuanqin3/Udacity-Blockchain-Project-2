f5643dad7f7e388520bddcb884173d664c337d32
ChuanQin <callforsky@gmail.com>
2018-11-14 22:43:27 -0500

    Completed the backbone
    Need to add error handling functions to async/await functions

--- simpleChain.js
+++ simpleChain.js
@@ -32,7 +32,6 @@ class Blockchain {
   constructor() {
     // check if we need to initiate the Genesis block
     this.getBlockHeight().then(result => {
-      console.log(result)
       if (result === -1) {
         console.log('Adding Genesis block now...')
         this.addBlock(this.createGenesisBlock())
@@ -58,8 +57,9 @@ class Blockchain {
       prevBlock = await this.getBlock(currentBlockHeight);
       newBlock.previousBlockHash = prevBlock.hash;
       newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
+    } else {
+      newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
     }
-    newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
 
     // register new block to the chain
     db2.addBlocktoChain(JSON.stringify(newBlock));
@@ -71,77 +71,63 @@ class Blockchain {
   // get block data
   async getBlock(blockHeight) {
     let block = await db2.getLevelDBData(blockHeight)
-    return block
+    return JSON.parse(block)
   }
 
   // get blockchain height
   async getBlockHeight() {
-    await db2.getBlockchainHeight()
+    return await db2.getBlockchainHeight()
   }
 
   // validate block
-  validateBlock(blockHeight){
-    return new Promise((resolve, reject) => {
-      // get block object
-      this.getBlock(blockHeight).then(block => {
-        // get block hash
-        let blockHash = block.hash;
-        // remove block hash to test block integrity
-        block.hash = '';
-        // generate block hash
-        let validBlockHash = SHA256(JSON.stringify(block)).toString();
-        // Compare
-        if (blockHash === validBlockHash) {
-          console.log('Block #'+blockHeight+' is valid.')
-          resolve(true);
-        } else {
-          console.log('Block #'+blockHeight+' invalid hash:\n'+blockHash+' <> '+validBlockHash);
-          resolve(false);
-        }
-      }).catch(err => {
-        console.log('Having problem with validating block# ' + blockHeight + 'Error: ' + err)
-        reject(false);
-      })
-    })
+  async validateBlock(blockHeight) {
+    let block = await this.getBlock(blockHeight)
+    // get block hash
+    let blockHash = block.hash;
+    // remove block hash to test block integrity
+    block.hash = '';
+    // generate block hash
+    let validBlockHash = SHA256(JSON.stringify(block)).toString();
+    // Compare
+    if (blockHash === validBlockHash) {
+      console.log('Block #'+blockHeight+' is valid.')
+      return true
+    } else {
+      console.log('Block #'+blockHeight+' invalid hash:\n'+blockHash+' <> '+validBlockHash);
+      return false
+    }
   }
 
-  // Validate blockchain
-  validateChain() {
+  // validate chain
+  async validateChain() {
     let errorLog = [];
-    this.getBlockHeight().then(blockchainHeight => {
-      // loop through blocks and validate one by one
-      for (var i = 0; i <= blockchainHeight; i++) {
-        this.validateBlock(i).then(result => {
-          // result is either true or false
-          if (!result) {
-            errorLog.push(i);
-          }
-          // compare blocks hash link; skip the last block, which has no next block
-          if (i === blockchainHeight) {
-            let currentHash = this.getBlock(i).hash;
-            let previousHash = this.getBlock(i+1).previousBlockHash;
-            Promise.all([currentHash, previousHash]).then(hashes => {
-              if (hashes[0] !== hashes[1]) {
-                errorLog.push(i);
-              }
-            });
-          }
-        });
+    let blockHeight = await this.getBlockHeight()
+    // loop through blocks and validate one after one
+    for (var i = 0; i <= blockHeight; i++) {
+      let validateResult = await this.validateBlock(i)
+      if (validateResult === false) {
+        errorLog.push(i);
       }
-      // report the result
-      if (errorLog.length > 0) {
-        console.log('Block errors = ' + errorLog.length);
-        console.log('Blocks: ' + errorLog);
-      } else {
-        console.log('No errors detected');
+      // compare blocks hash link; skip the last block, which has no next block
+      if (i < blockHeight) {
+        let currentHash = await this.getBlock(i).hash;
+        let previousHash = await this.getBlock(i+1).previousBlockHash;
+        if (currentHash !== previousHash) {
+          errorLog.push(i);
+        }
       }
-    }).catch(err => {
-      console.log('Having error with validating this chain. Error: ' + err);
-    });
+    }
+    // report the result
+    if (errorLog.length > 0) {
+      console.log('Block errors = ' + errorLog.length);
+      console.log('Blocks: ' + errorLog);
+    } else {
+      console.log('No errors detected');
+    }
   }
 }
 
-// Testing codes
+// Pre-set the testing codes in Node REPL
 let chain = new Blockchain();
 (function theLoop (i) {
   setTimeout(async function () {
